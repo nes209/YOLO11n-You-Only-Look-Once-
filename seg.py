dd231901b9ba7c3f493f030f46268d33ad6ac2cc
nesma Aljanad <aljanadn@gmail.com>
2025-02-19 20:49:53 +0300

    Add files via upload

---
+++ seg.py
@@ -0,0 +1,26 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO("yolo11n-seg.pt")
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    
+    success, frame = cap.read()
+    if success:
+        
+        results = model(frame)
+        
+        annotated_frame = results[0].plot()
+        
+        cv2.imshow("YOLO Inference", annotated_frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        
+        break
+
+cap.release()
+cv2.destroyAllWindows()
